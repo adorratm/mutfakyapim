10236bf78883dde8908b4256b3d17e83b261f2c8
Emre KILIÇ <emrekilic19983@gmail.com>
2023-03-21 09:49:59 +0300

    update config last

--- application/config/config.php
+++ application/config/config.php
@@ -461,7 +461,7 @@ $config['csrf_token_name'] = 'csrf_token';
 $config['csrf_cookie_name'] = 'csrf_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = FALSE;
-$config['csrf_exclude_uris'] = array('payment/make_payment');
+$config['csrf_exclude_uris'] = array('tr/odeme');
 
 /*
 |--------------------------------------------------------------------------
